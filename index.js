192bc352382a80543f048e903fa4abbefd860c17
Bhaas-kar <110971795+bhskar@users.noreply.github.com>
2023-04-27 00:58:01 +0530

    changes in index.js in this node js project

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/route1', // creates a route for the URL route (/route1)
+        function(req, res) {res.send('Hello World! from route 1!')}
+       )  // sends the response 'Hello World! from route 1!' back to the client
+       
 app.listen(port, () => {
   console.log(`This app now listening on port ${port}`)
 })
